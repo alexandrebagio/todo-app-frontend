3c6bd004ceb44033b72373d34427b9a21293278f
Alexandre Bagio da Luz <alexandre.luz@satc.edu.br>
2019-01-30 19:14:53 -0200

    todo-app-frontend

---
+++ src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa hisoria</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti quaerat tenetur architecto perspiciatis et cum saepe provident, veritatis fuga sit, rem tempora ducimus nulla, ratione ut hic! Modi, repellendus accusamus.</p>
+        <h2>Missão visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam obcaecati eveniet consequuntur consequatur libero porro labore asperiores modi optio. Quos iure quidem, magnam autem laboriosam exercitationem eligendi facilis perspiciatis saepe.</p>
+    </div>
+)
